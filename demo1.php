656fae3b771154465f7468be5283d64416ba6d47
Randale Mejia <randalelouie.mejia@sdca.edu.ph>
2024-10-05 14:36:26 +0800

    Update demo1.php

--- demo1.php
+++ demo1.php
@@ -3,35 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP DEMO</title>
 </head>
 <body>
-    <form action="output.php" method="Post">
+   
+    <?php
+
+
+    if (isset($_GET["error"])){
+    ?>
+        <p style="background-color: red; color:aliceblue"><?php echo $_GET["error"]?></p>  
+    <?php
+    }//echo $_GET["error"];
+    ?>
+       
+
+
+    <?php
+
+
+    if (isset($_GET["success"])){
+    ?>
+    <p style="background-color: green; color:aliceblue"><?php echo $_GET["success"]?></p>  
+    <?php
+    }//echo $_GET["error"];
+    ?>
+
+
+    <form action="output.php" method="POST">
         <div>
-        <label for="username">username</label>
-        <input type="text" name="username" id="username">
+        <label for = "username">Username</label>
+        <input type ="text" name="username" id="username">
         </div>
 
-        <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
-        </div>
 
         <div>
-        <label for="password1">Password</label>
-        <input type="password" name="password1" id="password1">
+        <label for = "email">Email</label>
+        <input type ="email" name="email" id="email">
         </div>
 
-        <div>
-        <label for=confirm-password>confirm password</label>
-        <input type="password" name="confirm-password" id="confirm-password">
-        </div>
 
         <div>
-        <input type="submit" value="register">
+        <label for = "password">Password</label>
+        <input type ="password" name="password" id="password">
         </div>
+
+
+
+
+        <div>
+        <label for = "confirm-password">Confirm Password</label>
+        <input type ="password" name="confirm-password" id="confirm-password">
+        </div>
+
+
+
+
+        <div>
+        <input type ="submit" value="Register">
+        </div>
+
+
+
 
     </form>
-
+   
 </body>
 </html>
